4dcea3679bc44c180dce3b9f682f175b342edb2f
Shavkat Khoshimov <khashimov023@gmail.com>
2025-09-10 11:08:52 +0500

    Initial Commit

---
+++ Noor/NoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoorApp.swift
+//  Noor
+//
+//  Created by Shavkat Khoshimov on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
